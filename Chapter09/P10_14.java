6af9c05856205c89067e2ee0c618acc87d45664a
springwind2006 <spring.wind2006@163.com>
2017-05-30 20:59:36 +0800

    init

---
+++ Chapter09/P10_14.java
@@ -0,0 +1,23 @@
+public class P10_14{
+	static int jieti(){
+		int i,res=0;
+		int count;
+		count=7;
+		for(i=1;i<=100;i++){
+			if((count%2==1) && (count%3==2) && (count%5==4) && (count%6==5) ){
+				res=count;
+				break;
+			}
+			count=7*(i+1);
+		}
+		return res;
+	}
+	public static void main(String[] args){
+		int num;
+		System.out.printf("爱因斯坦阶梯问题求解！\n");
+		num=jieti();
+		System.out.printf("最少共有%d个阶梯\n",num);
+	}
+
+
+}
